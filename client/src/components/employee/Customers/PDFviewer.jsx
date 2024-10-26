6c730ae57c8fcab5e7370505240b50a1e6084da3
ramtejvigna <vignaramtejtelagarapu@gmail.com>
2024-10-26 22:32:57 +0530

    PDF viewer updated

--- client/src/components/employee/Customers/PDFviewer.jsx
+++ client/src/components/employee/Customers/PDFviewer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FaDownload, FaWhatsapp, FaEnvelope } from 'react-icons/fa';
 
-const PDFViewer = ({ pdfUrl, handleDownload, handleSendMail, email, enabledRow, pdfId, onClose }) => {
+const PDFViewer = ({ pdfUrl, onClose }) => {
   return (
     <div className="fixed inset-0 flex justify-center items-center z-[1000] bg-black bg-opacity-75 backdrop-blur-md overflow-scroll scrollbar-hide">
       <div className="bg-white rounded-lg shadow-xl p-7 w-full mx-auto max-w-[800px] h-full max-h-[600px] relative">
@@ -15,33 +15,8 @@ const PDFViewer = ({ pdfUrl, handleDownload, handleSendMail, email, enabledRow, 
         </button>
 
         <h2 className="mb-4 text-xl font-semibold">Generated PDF:</h2>
-
-        {/* Button container */}
-        <div className="flex justify-end mb-4 space-x-2">
-          <button
-            onClick={() => handleDownload(pdfUrl, pdfId)}
-            
-            className="rounded-lg px-4 py-2 transition duration-200 "
-          >
-            <FaDownload />
-          </button>
-          <button
-            onClick={() => { /* Add your WhatsApp functionality here */ }}
-            className="rounded-lg px-4 py-2 transition duration-200"
-          >
-            <FaWhatsapp />
-          </button>
-          <button
-            onClick={() => handleSendMail(pdfUrl, pdfId, email)}
-            disabled={enabledRow !== pdfId}
-            className="rounded-lg px-4 py-2 transition duration-200"
-          >
-            <FaEnvelope />
-          </button>
-        </div>
-
         {/* PDF Iframe Viewer */}
-        <div className="flex items-center justify-center w-full h-[80%]">
+        <div className="flex items-center justify-center w-full h-[90%]">
           <iframe
             src={pdfUrl}
             className="w-full h-full border rounded-lg"
