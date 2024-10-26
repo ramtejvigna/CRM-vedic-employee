ed33524913fe34a69c16a8f44fb2fa08039fce51
ramtejvigna <vignaramtejtelagarapu@gmail.com>
2024-10-26 21:51:40 +0530

    Navbar - Removed search

--- client/src/components/employee/Navbar/index.jsx
+++ client/src/components/employee/Navbar/index.jsx
@@ -49,15 +49,6 @@ const Navbar = () => {
               </div>
             </div>
             <div className="ml-4 flex items-center space-x-4">
-              <div className="relative">
-                <input
-                  type="text"
-                  placeholder="Search..."
-                  className={`${isDarkMode ? 'bg-gray-800 text-gray-300' : 'bg-gray-100 text-gray-800'
-                    } pl-10 pr-4 py-3 rounded-xl focus:outline-none focus:ring-2 focus:ring-indigo-500`}
-                />
-                <FaSearch className="h-7 w-5 text-gray-500 absolute left-3 top-2.5" />
-              </div>
               <NotificationButton />
               <div className="relative">
                 {/* User Icon */}
