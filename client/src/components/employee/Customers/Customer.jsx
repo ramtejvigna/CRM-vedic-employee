0548954f0de748dbd5fa2c88b45ff79a33b2c7df
ramtejvigna <vignaramtejtelagarapu@gmail.com>
2024-11-01 22:40:54 +0530

    Customer section

--- client/src/components/employee/Customers/Customer.jsx
+++ client/src/components/employee/Customers/Customer.jsx
@@ -251,7 +251,7 @@ const Customer = () => {
                                 </p>
 
                                 <p className="text-gray-600 dark:text-gray-300 grid grid-cols-2 w-2/3"><strong>Payment Time:</strong> {customerDetails.paymentTime || 'N/A'}</p>
-                                {/* <p className="text-gray-600 dark:text-gray-300"><strong className='mr-1'>Payment Transaction ID:</strong> {customerDetails?.payTransactionID || 'N/A'}</p> */}
+                                {/* <p className="text-gray-600 dark:text-gray-300"><strong className='mr-1'>Payment Transaction ID:</strong> {customerDetails.payTransactionID || 'N/A'}</p> */}
                                 <p className="text-gray-600 dark:text-gray-300 grid grid-cols-2 w-2/3"><strong>Amount paid:</strong> {customerDetails?.amountPaid || 'N/A'}</p>
                             </div>
                         </div>
