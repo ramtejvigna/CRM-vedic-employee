78ce181b8a35a9aede71de17a228bd4e2a5af1f5
ramtejvigna <vignaramtejtelagarapu@gmail.com>
2024-11-14 20:58:03 +0530

    Removed uppercase for table headers

--- client/src/components/employee/Customers/index.jsx
+++ client/src/components/employee/Customers/index.jsx
@@ -98,7 +98,7 @@ export const Customers = () => {
               ].map((header) => (
                 <th
                   key={header}
-                  className="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider"
+                  className="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300 tracking-wider"
                 >
                   {header}
                 </th>
